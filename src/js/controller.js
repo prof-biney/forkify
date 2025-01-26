6922e2a25d6740cf8e1fdbaa805047865aa32520
abiney1321 <abiney1321@gmail.com>
2025-01-26 02:17:54 +0000

    showRecipe function

--- src/js/controller.js
+++ src/js/controller.js
@@ -12,3 +12,20 @@ const timeout = function (s) {
 // https://forkify-api.jonas.io
 
 ///////////////////////////////////////
+
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      'https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+    console.log(data);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+showRecipe();
