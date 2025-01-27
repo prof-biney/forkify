af1f9f002c3572c65f6b4784c026b5dc177d7add
abiney1321 <abiney1321@gmail.com>
2025-01-27 12:35:52 +0000

    setting up for MVC architecture

--- src/js/controller.js
+++ src/js/controller.js
@@ -31,12 +31,16 @@ const renderSpinner = function (parentEl) {
 
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+    console.log(id);
+
+    if (!id) return;
+
     // 1) Loading recipe
     renderSpinner(recipeContainer);
 
     const res = await fetch(
-      // 'https://forkify-api.jonas.io/api/v2/recipes/664c8f193e7aa067e94e863d'
-      'https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886'
+      `https://forkify-api.jonas.io/api/v2/recipes/${id}`
     );
     const data = await res.json();
 
@@ -159,4 +163,6 @@ const showRecipe = async function () {
   }
 };
 
-showRecipe();
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe));
+// window.addEventListener('hashchange', showRecipe);
+// window.addEventListener('load', showRecipe);
