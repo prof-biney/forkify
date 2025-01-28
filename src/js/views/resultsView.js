94c1a962e9fb47a986ac76e15a21f8091dff96ba
abiney1321 <abiney1321@gmail.com>
2025-01-28 15:16:50 +0000

    error message for results search

--- src/js/views/resultsView.js
+++ src/js/views/resultsView.js
@@ -3,6 +3,8 @@ import icons from 'url:../../img/icons.svg';
 
 class ResultsView extends View {
   _parentEl = document.querySelector('.results');
+  _errorMessage = 'No recipes found for your query! Please try again';
+  _message = '';
 
   _generateMarkup() {
     return this._data.map(this._generateMarkupPreview).join('');
