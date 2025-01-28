41072e3334cf9e2635d2818822519234f0e42b37
abiney1321 <abiney1321@gmail.com>
2025-01-28 15:16:00 +0000

    handling erors in parent class

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -4,8 +4,10 @@ export default class View {
   _data;
 
   render(data) {
-    this._data = data;
+    if (!data || (Array.isArray(data) && data.length === 0))
+      return this.renderError();
 
+    this._data = data;
     const markup = this._generateMarkup();
 
     this._clear();
